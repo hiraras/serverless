2d65c797e9d93045b6238941323f5a185162d3b9
hirara <kazilagui@163.com>
2023-06-30 16:22:21 +0800

    feat: 测试

--- api/oauth.js
+++ api/oauth.js
@@ -1,5 +1,9 @@
-export default function handler(req, res) {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
-  res.json({ name: "John Doe" });
+const axios = require("axios");
+
+export default async function handler(req, res) {
+  const response = await axios.get(
+    "https://api.twitter.com/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1687941198&oauth_nonce=MHmqnhbbTIS&oauth_version=1.0&oauth_signature=05ldMLLPLow5tNU9lNifQvg7qKM%3D"
+  );
+
+  res.json(response.data);
 }
