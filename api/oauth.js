14193c208f4e7a33840b6332ffd0ab43b7e7133a
hirara <kazilagui@163.com>
2023-06-30 16:25:12 +0800

    feat: update url

--- api/oauth.js
+++ api/oauth.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 export default async function handler(req, res) {
   const response = await axios.get(
-    "https://api.twitter.com/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1687941198&oauth_nonce=MHmqnhbbTIS&oauth_version=1.0&oauth_signature=05ldMLLPLow5tNU9lNifQvg7qKM%3D"
+    "https://api.twitter.com/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688095059&oauth_nonce=esEH0AtOw3w&oauth_version=1.0&oauth_signature=BpHxvHnZXap4b4YlODAsYzBbSr0%3D"
   );
 
   res.json(response.data);
