37ad93cec780235cd0f2e0fae6364d9a1a9e8428
hirara <kazilagui@163.com>
2023-07-01 16:12:17 +0800

    feat: test4

--- api/oauth.js
+++ api/oauth.js
@@ -5,16 +5,16 @@ export default async function handler(req, res) {
   //   "https://api.twitter.com/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688095059&oauth_nonce=esEH0AtOw3w&oauth_version=1.0&oauth_signature=BpHxvHnZXap4b4YlODAsYzBbSr0%3D"
   // );
 
+  // const options = {
+  //   hostname: "mcs.snssdk.com",
+  //   path: "/list",
+  //   method: "POST",
+  // };
   const options = {
-    hostname: "mcs.snssdk.com",
-    path: "/list",
+    hostname: "api.twitter.com",
+    path: "/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688198114&oauth_nonce=Uallw22Iqu9&oauth_version=1.0&oauth_signature=UAyrbRyY2y1lsY98VFmt7JA1GGE%3D",
     method: "POST",
   };
-  // const options = {
-  //   hostname: "api.twitter.com",
-  //   path: "/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688095059&oauth_nonce=esEH0AtOw3w&oauth_version=1.0&oauth_signature=BpHxvHnZXap4b4YlODAsYzBbSr0%3D",
-  //   method: "GET",
-  // };
 
   // 发送请求
   const request = http.request(options, (response) => {
