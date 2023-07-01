535f4cb7bb4c0b49e92fde110f61e059b6e4bdff
hirara <kazilagui@163.com>
2023-07-01 11:31:42 +0800

    feat: update

--- api/oauth.js
+++ api/oauth.js
@@ -9,12 +9,8 @@ export default async function handler(req, res) {
 
   const options = {
     hostname: "api.twitter.com",
-    port: 80,
     path: "/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688095059&oauth_nonce=esEH0AtOw3w&oauth_version=1.0&oauth_signature=BpHxvHnZXap4b4YlODAsYzBbSr0%3D",
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
   };
 
   // 发送请求
@@ -23,7 +19,6 @@ export default async function handler(req, res) {
 
     response.on("data", (chunk) => {
       data += chunk;
-      data += "1";
     });
 
     response.on("end", () => {
