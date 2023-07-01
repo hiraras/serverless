bb8cd9b24e38ffc5b2395cc9f368a2d1a39fd3cd
hirara <kazilagui@163.com>
2023-07-01 17:55:11 +0800

    feat: xx

--- api/oauth.js
+++ api/oauth.js
@@ -2,10 +2,15 @@ const http = require("http");
 
 export default async function handler(req, res) {
   const options = {
-    hostname: "api.twitter.com",
-    path: "/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688198114&oauth_nonce=Uallw22Iqu9&oauth_version=1.0&oauth_signature=UAyrbRyY2y1lsY98VFmt7JA1GGE%3D",
+    hostname: "api.meet48.xyz",
+    path: "/wallet/api/v1/user/getPublicKey",
     method: "POST",
   };
+  // const options = {
+  //   hostname: "api.twitter.com",
+  //   path: "/oauth/request_token?oauth_consumer_key=h8Pv7MaAptzKTtyml7Vag3WoO&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1688198114&oauth_nonce=Uallw22Iqu9&oauth_version=1.0&oauth_signature=UAyrbRyY2y1lsY98VFmt7JA1GGE%3D",
+  //   method: "POST",
+  // };
 
   // 发送请求
   const request = http.request(options, (response) => {
